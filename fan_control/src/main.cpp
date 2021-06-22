f74b5fed6bd884f39a9eb23846debc616ebb6e8f
Torsten Ruger <torsten@villataika.fi>
2021-06-22 15:51:58 +0300

    Split ota code out

--- fan_control/src/main.cpp
+++ fan_control/src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#include "ota.hpp"
 
 // DHT Temperature & Humidity Sensor
 // Unified Sensor Library Example
@@ -32,56 +29,7 @@ const char* ssid = "fan_XXX";
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
-  boolean result = WiFi.softAP(ssid);
-  while (result == false) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  // Port defaults to 3232
-  //  ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp3232-[MAC]
-  ArduinoOTA.setHostname("myesp32.local");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-    ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  ota_setup(ssid);
 
   // Initialize device.
   dht.begin();
@@ -114,6 +62,7 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();
+  Serial.println(WiFi.softAPIP());
 
   // Delay between measurements.
   delay(delayMS);
